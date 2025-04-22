bd86833e30f43d4c6e3a2fcc67cc14dab3a777a4
Abyssian <svirepiyork@mail.ru>
2025-04-22 03:37:34 +0300

    Urok3.1 and Urok3.2 done

---
+++ Urok3/Urok3.2.py
@@ -0,0 +1,13 @@
+# -*- coding: cp1251 -*-
+
+print("¬ведите этапы развити€ человека по пор€дку:")
+
+stage1 = input("1. ")
+stage2 = input("2. ")
+stage3 = input("3. ")
+stage4 = input("4. ")
+stage5 = input("5. ")
+stage6 = input("6. ")
+
+print("\nЁтапы развити€ человека:")
+print( "Dryopithecus", "Australopithecus", "Homo habilis", "Homo erectus", "Homo sapiens", "Homo sapiens sapiens", sep=" => ")
