67d25b83a70e823e521e37b8338afeda4a7c877e
Abyssian <svirepiyork@mail.ru>
2025-04-23 08:31:13 +0300

    add Urok9.1-9.3

---
+++ Urok9/Urok9.1.py
@@ -0,0 +1,7 @@
+﻿n = int(input())
+numbers = input().split()
+unique_numbers = set()
+for num in numbers:
+    unique_numbers.add(int(num))
+
+print(len(unique_numbers))
---
+++ Urok9/Urok9.2.py
@@ -0,0 +1,12 @@
+﻿n = int(input())
+set1 = set()
+for _ in range(n):
+    set1.add(int(input()))
+
+m = int(input())
+set2 = set()
+for _ in range(m):
+    set2.add(int(input()))
+common_elements = set1.intersection(set2)
+
+print(len(common_elements))
