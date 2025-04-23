ac8695661bad86f0dc856eaf3a25ee5f8e3ec5e5
Abyssian <svirepiyork@mail.ru>
2025-04-23 05:15:05 +0300

    add urok6.1-6.3

---
+++ Urok6/Urok6.2.py
@@ -0,0 +1,14 @@
+﻿X = int(input("Введите число: "))
+i = 1
+count = 0
+
+while i * i <= X:
+    if X % i == 0:
+        if i == X // i:
+            count += 1
+        else:
+            count += 2
+    i += 1
+
+print(count)
+#итого у 2 миллиарда 110 натуральных делителей
