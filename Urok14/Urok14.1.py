f932d50d8797e2ac365d2dd978f59e23b268b5e5
Abyssian <svirepiyork@mail.ru>
2025-04-24 01:45:11 +0300

    add Urok14.1

---
+++ Urok14/Urok14.1.py
@@ -0,0 +1,13 @@
+﻿import sys
+sys.setrecursionlimit(2000)
+
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+def print_list(index=0):
+    if index < len(my_list):
+        print(my_list[index])
+        print_list(index + 1)
+    else:
+        print("Конец списка")
+
+print_list()
