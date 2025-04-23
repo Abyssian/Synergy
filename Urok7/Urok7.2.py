6815ef780e5aa08420156bfdff389760091e47a2
Abyssian <svirepiyork@mail.ru>
2025-04-23 06:00:28 +0300

    add Urok7.1-7.2

---
+++ Urok7/Urok7.2.py
@@ -0,0 +1,18 @@
+﻿text = input("Введите строку: ")
+
+if len(text) > 1000:
+    print("Ошибка: строка превышает допустимую длину")
+else:
+    result = ""
+    probel = False
+
+    for char in text:
+        if char == " ":
+            if not probel:
+                result += char
+                probel = True
+        else:
+            result += char
+            probel = False
+
+    print(result)
